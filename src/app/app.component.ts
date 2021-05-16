2ed945eb7b1ea0341c903acd862f19f6eee6ed47
unknown <G00385842@gmit.ie>
2021-05-16 19:02:39 +0100

    Empty first commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    //routing the button of the nav bar to the page i want them to go to  
     { title: 'Movies', url: '/movie' },
     { title: 'About & Contact', url: '/about' },
     { title: 'Feedback', url: '/feedback'},
